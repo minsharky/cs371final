404bd2a6ee5b6b2bda9a04da3b05e8fa787391c4
Minseo Kim <76415166+minsharky@users.noreply.github.com>
2024-02-14 21:16:40 -0600

    retrieve all universities

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 # Set up the SPARQL endpoint URL
-sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
 
 # Define the SPARQL query
-sparql.setQuery("""
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    SELECT ?label
-    WHERE {
-        <http://dbpedia.org/resource/Python_(programming_language)> rdfs:label ?label .
-        FILTER (langMatches(lang(?label), "en"))
-    }
-""")
+query = """
+SELECT ?universityLabel
+WHERE {
+  ?university wdt:P31 wd:Q3918;  # Instances of all universities
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+}
+"""
 
-# Specify the format of the results (optional)
+# Set the query string
+sparql.setQuery(query)
+
+# Specify the format of the results
 sparql.setReturnFormat(JSON)
 
 # Execute the query and retrieve the results
@@ -21,4 +23,4 @@ results = sparql.query().convert()
 
 # Process the results
 for result in results["results"]["bindings"]:
-    print(result["label"]["value"])
+    print(result["universityLabel"]["value"])
