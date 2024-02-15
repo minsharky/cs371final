9e507ec26f5c36b1d53d974af106a4455ecb82de
Minseo Kim <76415166+minsharky@users.noreply.github.com>
2024-02-14 20:22:32 -0600

    init commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+# Set up the SPARQL endpoint URL
+sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+
+# Define the SPARQL query
+sparql.setQuery("""
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    SELECT ?label
+    WHERE {
+        <http://dbpedia.org/resource/Python_(programming_language)> rdfs:label ?label .
+        FILTER (langMatches(lang(?label), "en"))
+    }
+""")
+
+# Specify the format of the results (optional)
+sparql.setReturnFormat(JSON)
+
+# Execute the query and retrieve the results
+results = sparql.query().convert()
+
+# Process the results
+for result in results["results"]["bindings"]:
+    print(result["label"]["value"])
