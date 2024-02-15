52b82c689c1ca8ccd1b64cf6b2d5e2d4bb0f8fa6
WeiViv <100058931+WeiViv@users.noreply.github.com>
2024-02-14 21:44:37 -0600

    Update python prompt.py

--- python prompt.py
+++ python prompt.py
@@ -47,101 +47,101 @@ def prompt():
 # main
 #
 
-  print('** Welcome to Trip Planner App **')
-  print()
+print('** Welcome to Trip Planner App **')
+print()
 
-  # eliminate traceback so we just get error message:
-  sys.tracebacklimit = 0
+# eliminate traceback so we just get error message:
+sys.tracebacklimit = 0
 
-  #
-  # what config file should we use for this session?
-  #
-  config_file = 'client-config.ini'
+#
+# what config file should we use for this session?
+#
+config_file = 'client-config.ini'
 
-  #
-  # setup base URL to web service:
-  #
-  configur = ConfigParser()
-  configur.read(config_file)
-  baseurl = configur.get('client', 'webservice')
+#
+# setup base URL to web service:
+#
+configur = ConfigParser()
+configur.read(config_file)
+baseurl = configur.get('client', 'webservice')
 
-  lastchar = baseurl[len(baseurl) - 1]
-  if lastchar == "/":
-    baseurl = baseurl[:-1]
+lastchar = baseurl[len(baseurl) - 1]
+if lastchar == "/":
+baseurl = baseurl[:-1]
 
-  #
-  # main processing loop:
-  #
+#
+# main processing loop:
+#
 
-  # handle user sign in or create new user
-  username = sign_in_or_create_user(baseurl)
+# handle user sign in or create new user
+username = sign_in_or_create_user(baseurl)
 
-  cmd = prompt()
+cmd = prompt()
 
-  while cmd != 0:
-    if cmd == 1:
-      # add-location
-      location_name = input("Enter location name> ").strip().lower()
-      cost = input("Enter cost of this activity> ")
-      try:
-        cost = float(cost)
-      except ValueError:
-        print("Invalid cost, cost can only be numeric value.")
-        continue
-      add_location(baseurl, location_name, cost, username)
+while cmd != 0:
+if cmd == 1:
+    # add-location
+    location_name = input("Enter location name> ").strip().lower()
+    cost = input("Enter cost of this activity> ")
+    try:
+    cost = float(cost)
+    except ValueError:
+    print("Invalid cost, cost can only be numeric value.")
+    continue
+    add_location(baseurl, location_name, cost, username)
 
-    elif cmd == 2:
-      # get-locations
-      get_locations(baseurl)
+elif cmd == 2:
+    # get-locations
+    get_locations(baseurl)
 
-    elif cmd == 3:
-      # get-unrated-locations
-      get_unrated_locations(baseurl, username)
+elif cmd == 3:
+    # get-unrated-locations
+    get_unrated_locations(baseurl, username)
 
-    elif cmd == 4:
-      # add-rating
-      location_name = input("Enter location name> ").strip().lower()
-      rating = input("Enter your rating for this location/activity (0~5)> ")
-      try:
-        rating = float(rating)
-      except ValueError:
-        print("Invalid rating, rating can only be numeric value.")
-        continue
-      add_rating(baseurl, location_name, rating, username)
+elif cmd == 4:
+    # add-rating
+    location_name = input("Enter location name> ").strip().lower()
+    rating = input("Enter your rating for this location/activity (0~5)> ")
+    try:
+    rating = float(rating)
+    except ValueError:
+    print("Invalid rating, rating can only be numeric value.")
+    continue
+    add_rating(baseurl, location_name, rating, username)
 
-    elif cmd == 5:
-      # calculate itinerary
-      num_locations = int(input("Enter the number of locations to visit> "))
-      if num_locations <= 0:
-        print(
-            "Invalid number of locations, number of locations must be greater than 0. Restarting calculate itinerary..."
-        )
-        continue
-      max_total_cost = int(
-          input("Enter your maximum budget for the entire trip> "))
-      if max_total_cost < 0:
-        print(
-            "Invalid maximum budget, maximum budget must be non-negative. Restarting calculate itinerary..."
-        )
-        continue
-      calculate_itinerary(baseurl, num_locations, max_total_cost)
+elif cmd == 5:
+    # calculate itinerary
+    num_locations = int(input("Enter the number of locations to visit> "))
+    if num_locations <= 0:
+    print(
+        "Invalid number of locations, number of locations must be greater than 0. Restarting calculate itinerary..."
+    )
+    continue
+    max_total_cost = int(
+        input("Enter your maximum budget for the entire trip> "))
+    if max_total_cost < 0:
+    print(
+        "Invalid maximum budget, maximum budget must be non-negative. Restarting calculate itinerary..."
+    )
+    continue
+    calculate_itinerary(baseurl, num_locations, max_total_cost)
 
-    elif cmd == 6:
-      reset(baseurl)
-      print()
-      print(
-          "*** Thanks for using Trip Planner App! Hope to see you again! :) ***"
-      )
-      sys.exit(0)
+elif cmd == 6:
+    reset(baseurl)
+    print()
+    print(
+        "*** Thanks for using Trip Planner App! Hope to see you again! :) ***"
+    )
+    sys.exit(0)
 
-    else:
-      print("** Unknown command, try again...")
+else:
+    print("** Unknown command, try again...")
 
-    cmd = prompt()
+cmd = prompt()
 
-  #
-  # done
-  #
-  print()
-  print('** done **')
-  sys.exit(0)
+#
+# done
+#
+print()
+print('** done **')
+sys.exit(0)
