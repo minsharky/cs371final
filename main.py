768418609b11cfb1aaf65ccf789d24ce346c08a2
Minseo Kim <76415166+minsharky@users.noreply.github.com>
2024-02-21 22:02:24 -0600

    language used

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
+from rdflib import Graph, Namespace, Literal, URIRef
+from rdflib.namespace import RDF, RDFS
+
+# Create an RDF graph
+g = Graph()
 
 # Set up the SPARQL endpoint URL
 sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
 
-# Define the SPARQL query
+# Language Used
 sparql.setQuery("""
-    SELECT ?universityLabel
+    SELECT ?university ?universityLabel ?language ?languageLabel
 WHERE {
   ?university 
       wdt:P31 wd:Q3918;  # Instances of all universities
+      wdt:P17 wd:Q30;
+      wdt:P2936 ?language.      # City of the university
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
 }
 
@@ -19,8 +26,15 @@ sparql.setReturnFormat(JSON)
 
 # Execute the query and retrieve the results
 results = sparql.query().convert()
-for r 
-print(results.keys)
-# Process the results
-# for result in results["results"]["bindings"]:
-#     print(result["universityLabel"]["value"])
+
+# Process the JSON results and convert them to RDF triples
+# Language Used
+for result in results["results"]["bindings"]:
+    # Add RDF triples to the graph
+    subject_uri = URIRef(result["university"]["value"])
+    predicate_uri = URIRef('http://www.wikidata.org/prop/direct/P2936')
+    object_uri = URIRef("http://www.wikidata.org/entity/"+result["language"]["value"])
+    g.add((subject_uri, predicate_uri, object_uri))
+
+for triple in g:
+    print(triple)
