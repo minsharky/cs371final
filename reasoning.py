30189f23facaaba935680c5adb5121fdaa2da868
Minseo Kim <76415166+minsharky@users.noreply.github.com>
2024-02-28 20:29:20 -0600

    init commit

--- reasoning.py
+++ reasoning.py
@@ -8,32 +8,57 @@ file_path = "./graph_file.ttl"
 
 # Create a new graph instance
 g = Graph()
+graph = Graph()
 
 # Parse the graph from the file
 g.parse(file_path, format="turtle")
+graph.parse(file_path, format="turtle")
 
-# for subject, predicate, obj in g:
-#     print("University:", subject)
-#     print("Predicate:", predicate)
-#     print("Object:", obj)
-#     print()
-
-q1 = prepareQuery("""
-    SELECT DISTINCT ?university
+q0 = prepareQuery("""
+    SELECT ?predicate ?value
     WHERE {
-        ?university ?predicate1 ?universityType .
-        FILTER(?predicate1 = "universityTypeLabel")
-        FILTER(?universityType = "Private")
+        ?university ?predicate ?value.
+        FILTER(?university = <http://www.wikidata.org/entity/Q7988285>)
     }
 """, initNs={"xsd": XSD})
 
-q2 = prepareQuery("""
+member, admission, location, language, student, calendar, type, found = None, None, None, None, None, None, None, None
+
+for row in graph.query(q0):
+    pred = str(row.predicate)
+    if(pred == "memberOf"):
+        member = row.value.n3()
+    if(pred == "admissionRate"):
+        admission = row.value
+    if(pred == "location"):
+        location = row.value.n3()
+    if(pred == "language"):
+        language = (row.value).n3()
+    if(pred == "studentCount"):
+        student = row.value
+    if(pred == "calendar"):
+        calendar = row.value.n3()
+    if(pred == "universityTypeLabel"):
+        type = str(row.value)
+    if(pred == "founded"):
+        found = row.value
+
+q1 = prepareQuery(f"""
     SELECT DISTINCT ?university
-    WHERE {
+    WHERE {{
+        ?university ?predicate1 ?universityType .
+        FILTER(?predicate1 = "universityTypeLabel")
+        {'FILTER(?universityType = "' + type + '")' if type is not None else ''}
+    }}
+""", initNs={"xsd": XSD})
+
+q2 = prepareQuery(f"""
+    SELECT DISTINCT ?university
+    WHERE {{
         ?university ?predicate2 ?language .
         FILTER(?predicate2 = "language")
-        FILTER(?language = <http://www.wikidata.org/entity/Q1860>)
-    }
+        {'FILTER(?language = "' + language + '")' if language is not None else ''}
+    }}
 """, initNs={"xsd": XSD})
 
 q3 = prepareQuery("""
@@ -47,23 +72,23 @@ q3 = prepareQuery("""
     }
 """, initNs={"xsd": XSD})
 
-q4 = prepareQuery("""
+q4 = prepareQuery(f"""
     SELECT DISTINCT ?university
-    WHERE {
+    WHERE {{
         ?university ?predicate4 ?admissionRate .
         FILTER(?predicate4 = "admissionRate")
         FILTER (datatype(?admissionRate) = xsd:decimal)
         FILTER(?admissionRate >= 0.06 && ?admissionRate <= 0.15)
-    }
+    }}
 """, initNs={"xsd": XSD})
 
-q5 = prepareQuery("""
+q5 = prepareQuery(f"""
     SELECT DISTINCT ?university
-    WHERE {
+    WHERE {{
         ?university ?predicate5 ?memberOf .
         FILTER(?predicate5 = "memberOf")
-        FILTER(?memberOf = <http://www.wikidata.org/entity/Q49113>)
-    }
+        {'FILTER(?memberOf = "' + member + '")' if member is not None else ''}
+    }}
 """, initNs={"xsd": XSD})
 
 q6 = prepareQuery("""
@@ -76,19 +101,40 @@ q6 = prepareQuery("""
     }
 """, initNs={"xsd": XSD})
 
+q7 = prepareQuery(f"""
+    SELECT DISTINCT ?university
+    WHERE {{
+        ?university ?predicate7 ?location .
+        FILTER(?predicate7 = "location")
+        {'FILTER(?location = "' + location + '")' if location is not None else ''}
+    }}
+""", initNs={"xsd": XSD})
+
+q8 = prepareQuery(f"""
+    SELECT DISTINCT ?university
+    WHERE {{
+        ?university ?predicate7 ?calendar .
+        FILTER(?predicate7 = "calendar")
+        {"FILTER(?calendar = " + calendar + ")" if calendar is not None else ""}
+    }}
+""", initNs={"xsd": XSD})
+
+
 filteredUniversities = set()
 
 for row in g.query(q1):
     filteredUniversities.add(row.university)
 
-q = [q2, q3, q4, q5, q6]
+q = [[q2, language], [q3, student], [q4, admission], [q5, member], [q6, found], [q7, location], [q8, calendar]]
 
-for tempQ in q:
+for tempQ, tempV in q:
     tempSet = set()
-    for row in g.query(tempQ):
-        if row.university in filteredUniversities:
-            tempSet.add(row.university)
-    filteredUniversities = tempSet
+    if(tempV is not None):
+        for row in g.query(tempQ):
+            if row.university in filteredUniversities:
+                tempSet.add(row.university)
+        if(len(tempSet) != 0):
+            filteredUniversities = tempSet
 
 for uni in filteredUniversities:
     print(f"University that meet the criteria: {uni}")
